387ccf32a53a30fe6383d8067db5c51a24b3e6f4
uharston <uharston@gmail.com>
2020-12-08 15:42:49 -0600

    Add footer | Center Image

--- pages/index.js
+++ pages/index.js
@@ -25,14 +25,17 @@ export default function Home() {
           </Navbar.Collapse>
         </Navbar>
         </nav>
-        <div className='landing-image'>
+        <div className={styles.imageContainer}>
           <Image
+            className={styles.landingImage}
             src="/images/world-map-night.jpg"
             alt="Picture of the author"
-            layout='responsive'
-            width={1000}
-            height={500}
-            priority={true}
+            layout='fill'
+            objectFit='cover'
+            // objectPosition='50% 50000%'
+            // width={1000}
+            // height={500}
+            // priority={true}
           />
         </div>
         <section className='about'>
@@ -60,7 +63,20 @@ export default function Home() {
           </Container>
         </section>
 
-        <footer>
+        <footer className={styles.sectionFooter}>
+          <Container>
+            <Row>
+              <Col xs={12} md={9}>
+                <p>Learn More</p>
+                <p>Privacy Policy</p>
+              </Col>
+              <Col xs={12} md={3}>
+                <h4 className='text-right'>Mbouke</h4>
+                <p className='text-right'>Connecting Workers</p>
+                <p className='text-right'>All Rights Reserved 2020</p>
+              </Col>
+            </Row>
+          </Container>
 
         </footer>
       </main>
