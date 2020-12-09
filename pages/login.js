c0afe300b905141924b8075fb46a74a5cc73baa3
uharston <uharston@gmail.com>
2020-12-09 15:26:55 -0600

    Responsive Image

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,5 @@
 import {Container, Nav, Navbar, Row, Col, Form, Button } from 'react-bootstrap'
+import Image from 'next/image'
 import styles from '../styles/Utils.module.sass'
 import Navigation from '../components/navigation.js'
 
@@ -11,28 +12,40 @@ function Login() {
             <div className={styles.content}>
                 <Container>
                     <Row>
-                        <Col xs={12} md={7} ></Col>
-                        <Col xs={12} md={5} >
-                            <h1>Login For Pros</h1>
-                            <Form>
-                                <Form.Group controlId="formBasicEmail">
-                                    <Form.Label>Email address</Form.Label>
-                                    <Form.Control type="email" placeholder="Enter email" />
-                                    <Form.Text className="text-muted">
-                                    We'll never share your email with anyone else.
-                                    </Form.Text>
-                                </Form.Group>
-                                <Form.Group controlId="formBasicPassword">
-                                    <Form.Label>Password</Form.Label>
-                                    <Form.Control type="password" placeholder="Password" />
-                                </Form.Group>
-                                <Form.Group controlId="formBasicCheckbox">
-                                    <Form.Check type="checkbox" label="Check me out" />
-                                </Form.Group>
-                                <Button variant="primary" type="submit">
-                                    Submit
-                                </Button>
-                            </Form>
+                        <Col xs={12} md={7} className="d-none d-md-block" >
+                        <Image
+                            className={styles.landingImage}
+                            src="/images/yard-worker.jpg"
+                            alt="Picture a professional trimming plants with an electric trimmer"
+                            layout='responsive'
+                            width={640}
+                            height={469}
+                        />
+                        </Col>
+                        <Col className={styles.formColumn} xs={12} md={5} >
+                       
+                                <div className={styles.formContainer}>
+                                <h1>Login For Pros</h1>
+                                <Form>
+                                    <Form.Group controlId="formBasicEmail">
+                                        <Form.Label>Email address</Form.Label>
+                                        <Form.Control type="email" placeholder="Enter email" />
+                                        <Form.Text className="text-muted">
+                                        We'll never share your email with anyone else.
+                                        </Form.Text>
+                                    </Form.Group>
+                                    <Form.Group controlId="formBasicPassword">
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control type="password" placeholder="Password" />
+                                    </Form.Group>
+                                    <Form.Group controlId="formBasicCheckbox">
+                                        <Form.Check type="checkbox" label="Check me out" />
+                                    </Form.Group>
+                                    <Button variant="primary" type="submit">
+                                        Submit
+                                    </Button>
+                                </Form>
+                            </div>
                         </Col>
                     </Row>
                 </Container>
