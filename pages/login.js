6a88e357bfd59d09b4b89c91ef26abce1dfb6aac
uharston <uharston@gmail.com>
2020-12-09 13:42:37 -0600

    Create Login Component

---
+++ pages/login.js
@@ -0,0 +1,12 @@
+import Footer from '../components/footer.js'
+
+function Login() {
+    return (
+        <div>
+            Login
+            <Footer />
+        </div>
+    )
+}
+
+export default Login
