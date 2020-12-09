af8f620c8e804c18d53ec11320cd9402a35ce226
uharston <uharston@gmail.com>
2020-12-09 14:47:56 -0600

    Import Navigation | Create Login Form

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,42 @@
+import {Container, Nav, Navbar, Row, Col, Form, Button } from 'react-bootstrap'
+import styles from '../styles/Utils.module.sass'
+import Navigation from '../components/navigation.js'
+
 import Footer from '../components/footer.js'
 
 function Login() {
     return (
         <div>
-            Login
+            <Navigation />
+            <div className={styles.content}>
+                <Container>
+                    <Row>
+                        <Col xs={12} md={7} ></Col>
+                        <Col xs={12} md={5} >
+                            <h1>Login For Pros</h1>
+                            <Form>
+                                <Form.Group controlId="formBasicEmail">
+                                    <Form.Label>Email address</Form.Label>
+                                    <Form.Control type="email" placeholder="Enter email" />
+                                    <Form.Text className="text-muted">
+                                    We'll never share your email with anyone else.
+                                    </Form.Text>
+                                </Form.Group>
+                                <Form.Group controlId="formBasicPassword">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type="password" placeholder="Password" />
+                                </Form.Group>
+                                <Form.Group controlId="formBasicCheckbox">
+                                    <Form.Check type="checkbox" label="Check me out" />
+                                </Form.Group>
+                                <Button variant="primary" type="submit">
+                                    Submit
+                                </Button>
+                            </Form>
+                        </Col>
+                    </Row>
+                </Container>
+                </div>
             <Footer />
         </div>
     )
