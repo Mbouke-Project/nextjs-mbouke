c8fba72b44c850d407d8551436c8c180a575cffe
uharston <uharston@gmail.com>
2020-12-09 14:29:49 -0600

    Import Navigation

--- pages/signup.js
+++ pages/signup.js
@@ -1,9 +1,12 @@
+import Navigation from '../components/navigation.js'
+
 import Footer from '../components/footer.js'
 
 
 function Signup() {
     return (
         <div>
+            <Navigation />
             Sign Up
             <Footer />
 
