5da07486542bd6bfde185cdae6dfde514f694927
uharston <uharston@gmail.com>
2020-12-09 16:40:07 -0600

    Customize Form

--- pages/signup.js
+++ pages/signup.js
@@ -16,22 +16,26 @@ function Signup() {
                         <Col xs={12} md={5} className={styles.signupContainer}>
                             <h2>Join Our Network Of Pros</h2>
                             <Form>
-                                <Form.Group contropd="formBasicEmail">
-                                    <Form.Label>Email address</Form.Label>
-                                    <Form.Control type="email" placeholder="Enter email" />
-                                    <Form.Text className="text-muted">
-                                    We'll never share your email with anyone else.
-                                    </Form.Text>
+                                <Form.Row>
+                                    <Col>
+                                        <Form.Group contropd="formBasicEmail">
+                                            <Form.Label>Country</Form.Label>
+                                            <Form.Control type="text" placeholder="Enter Country" />
+                                        </Form.Group>
+                                    </Col>
+                                    <Col>
+                                        <Form.Group >
+                                            <Form.Label>City</Form.Label>
+                                            <Form.Control type="text" placeholder="City" />
+                                        </Form.Group>
+                                    </Col>
+                                </Form.Row>
+                                <Form.Group >
+                                    <Form.Label>What Type Of Services Do You Provide?</Form.Label>
+                                    <Form.Control type="text" placeholder="e.g. Plumbering, Roofing, etc" />
                                 </Form.Group>
-                                <Form.Group contropd="formBasicPassword">
-                                    <Form.Label>Password</Form.Label>
-                                    <Form.Control type="password" placeholder="Password" />
-                                </Form.Group>
-                                <Form.Group contropd="formBasicCheckbox">
-                                    <Form.Check type="checkbox" label="Check me out" />
-                                </Form.Group>
-                                <Button variant="primary" type="submit">
-                                    Submit
+                                <Button variant="success" type="submit">
+                                    Get Started
                                 </Button>
                             </Form>
                         </Col>
