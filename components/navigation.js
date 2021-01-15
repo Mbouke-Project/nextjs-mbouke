845715accc746ae60549d290075c0ab205c2e468
uharston <uharston@gmail.com>
2021-01-15 17:33:54 -0600

    Add: Logout Btn and Conditional Display

--- components/navigation.js
+++ components/navigation.js
@@ -1,9 +1,43 @@
+import { useRouter } from 'next/router'
 import styles from '../styles/Home.module.sass'
 import Link from 'next/link'
 import { Nav, Navbar } from 'react-bootstrap'
 
 
 function Navigation() {
+  const router = useRouter()
+
+  const handleLogout = () => {
+    localStorage.token = ""
+    router.push('/')
+  }
+
+  const displayLogin = () => {
+    if (!!localStorage.token) {
+      return (
+        <Nav.Link>
+        <button onClick={ () => handleLogout(router) }className={styles.navLinks}>
+          Logout
+        </button>
+      </Nav.Link>
+      )
+    }
+    return (
+      <>
+        <Nav.Link>
+          <Link href='/signup' className={styles.navLinks}>
+            Sign Up
+          </Link>
+        </Nav.Link>
+        <Nav.Link >
+          <Link href='/login'>
+            Log In
+          </Link>
+        </Nav.Link>
+      </>
+    )
+  }
+  
     return (
         <nav>
         <Navbar collapseOnSelect expand="md"  variant="light" >
@@ -11,19 +45,17 @@ function Navigation() {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ml-auto">
-                
               <Nav.Link>
-                <Link href='/signup' className={styles.navLinks}>
-                  Sign Up
+                <Link href='/nextSignup' className={styles.navLinks}>
+                  Next Sign Up
                 </Link>
               </Nav.Link>
-                
-              <Nav.Link >
-                <Link href='/login'>
-                Log In
+              <Nav.Link>
+                <Link href='/myPage' className={styles.navLinks}>
+                  My Page
                 </Link>
               </Nav.Link>
-              
+              {displayLogin()}
             </Nav>
           </Navbar.Collapse>
         </Navbar>
