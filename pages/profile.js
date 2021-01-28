85d36018a84584dfe7041703d92470b1a7232c36
uharston <uharston@gmail.com>
2021-01-28 09:04:06 -0600

    Add: Extra state

--- pages/profile.js
+++ pages/profile.js
@@ -8,6 +8,9 @@ import Navigation from '../components/navigation.js'
 function Profile({ success }) {
   const router = useRouter()
   const [email, setEmail] = useState('')
+  const [country, setCountry ] = useState('')
+  const [city, setCity] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState('')
 
   useEffect(() => {
 
@@ -18,6 +21,9 @@ function Profile({ success }) {
       .then(response => {
         console.log(response)
         setEmail(response.data.user.email)
+        setCountry(response.data.user.country)
+        setCity(response.data.user.city)
+        setPhoneNumber(response.data.user.phone_number)
       })
       .catch( error => {
         console.log(error)
@@ -30,7 +36,10 @@ function Profile({ success }) {
 
     return <>
     <Navigation/>
- Welcome {email}
+      <h2>Welcome {email}</h2>
+      <p>Country: {country}</p>
+      <p>City: {city}</p>
+      <p>Phone Number: {phoneNumber}</p>
     </>
 }
 
