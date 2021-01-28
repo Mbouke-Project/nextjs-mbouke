62618d08177fae586adbdb454617a272af8ad85d
uharston <uharston@gmail.com>
2021-01-28 09:03:30 -0600

    Add: Session Provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
+import { Provider } from 'next-auth/client'
 import '../styles/globals.sass'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <Provider session={pageProps.session}>
+    <Component {...pageProps} />
+  </Provider>
+  )
 }
 
 export default MyApp
