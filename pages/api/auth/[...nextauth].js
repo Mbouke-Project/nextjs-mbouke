302ac084df2154b6852bc23754635a306fcf1bd9
uharston <uharston@gmail.com>
2021-02-04 13:18:32 -0600

    Tweak oauth routes

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,12 +7,20 @@ const options = {
             clientId: process.env.GITHUB_CLIENT_ID, 
             clientSecret: process.env.GITHUB_CLIENT_SECRET
         }),
+
     ], 
     debug: process.env.NODE_ENV === 'development', 
     secret: process.env.AUTH_SECRET, 
     jwt: {
         secret: process.env.JWT_SECRET,
-    }
+    }, 
+    pages: {
+        signIn: '/auth/signin',
+        signOut: '/auth/signout',
+        error: '/auth/error', // Error code passed in query string as ?error=
+        verifyRequest: '/auth/verify-request', // (used for check email message)
+        newUser: null // If set, new users will be directed here on first sign in
+      }
 }
 
 
