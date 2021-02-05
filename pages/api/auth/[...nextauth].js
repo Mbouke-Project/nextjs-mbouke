2cb8c38d30fd0287bbe5347726eddff8c7f80948
uharston <uharston@gmail.com>
2021-02-05 08:23:09 -0600

    Add: Rails login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+import axios from 'axios'
 
 const options = {
     providers: [
@@ -8,19 +9,41 @@ const options = {
             clientSecret: process.env.GITHUB_CLIENT_SECRET
         }),
 
+        Providers.Credentials({
+            name: 'Rails', 
+            credentials: {
+                email: { label: 'Email', type: 'email', placeholder: 'Email'}, 
+                password: { label: "Password", type: 'password', placeholder: 'Password'}
+            }, 
+            async authorize(credentials) {
+                // Add logic here to look up the user from the credentials supplied
+                const user = await fetchUser(credentials)
+                console.log('user', user)
+                if (user) {
+                    // Any object returned will be saved in `user` property of the JWT
+                    return user
+                } else {
+                    // If you return null or false then the credentials will be rejected
+                    // throw new Error('Auth Failed. Sorry.') // Redirect to error page
+                    // throw '/path/to/redirect'        // Redirect to a URL
+                    return null
+                    // You can also Reject this callback with an Error or with a URL:
+                }
+            
+            }
+        })
     ], 
     debug: process.env.NODE_ENV === 'development', 
     secret: process.env.AUTH_SECRET, 
     jwt: {
         secret: process.env.JWT_SECRET,
     }, 
-    pages: {
-        signIn: '/auth/signin',
-        signOut: '/auth/signout',
-        error: '/auth/error', // Error code passed in query string as ?error=
-        verifyRequest: '/auth/verify-request', // (used for check email message)
-        newUser: null // If set, new users will be directed here on first sign in
-      }
+}
+
+const fetchUser = async credentials => {
+    const result = await axios.post('http://127.0.0.1:4000/login', {user: credentials}, {withCredentials: true})
+    return result.data.user
+
 }
 
 
